73e90094c8c37a165010fac2336f68c91369426b
YoungWoong22 <hwa8078678@naver.com>
2022-10-04 17:53:35 +0900

    로그인창 - 로그인 오류시 오류 메시지 서버로 로그인정보넘기기

---
+++ stussy-clone-20220930-leeyw/src/main/resources/static/js/account/register.js
@@ -0,0 +1,30 @@
+const registerButton = document.querySelector(".account-button");
+
+registerButton.onclick = () => {
+    const accountInput = document.querySelectorAll(".account-input");
+
+    let user = {
+        lastName: accountInput[0].value,
+        firstName: accountInput[1].value,
+        email: accountInput[2].value,
+        password: accountInput[3].value
+    }
+    
+    //JSON.stringify() -> js 객체를 JSON문자열로 변환
+    //JON.parse()      -> JSON 문자열을 js객체로 변환
+
+    $.ajax({
+        async: false,                       //필수
+        type: "post",                       //필수
+        url: "/api/account/register",       //필수
+        contentType: "application/json",    //전송데이터가 json인 경우
+        data: JSON.stringify(user),         //전송할 데이터가 있으면    
+        dataType: "json",                   //json 외 text 등을 사용할 수 있지만 json
+        success: (response) => {            //성공시에 실행될 메소드
+            alert("회원가입 요청 성공");
+        },
+        error: (error) => {             //실패시 실행될 메소드
+            alert("회원가입 요청 실패");
+        }
+    }
+)};
---
+++ stussy-clone-20220930-leeyw/src/test/java/com/stussy/stussyclone20220930leeyw/StussyClone20220930LeeywApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclone20220930leeyw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StussyClone20220930LeeywApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
